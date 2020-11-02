1e147fa1d76038d8d3d87a274ebca35cdd8f0ca8
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-11-02 15:53:16 +0100

    Application context in AppController.java

--- src/main/java/com/example/demo/controllers/AppController.java
+++ src/main/java/com/example/demo/controllers/AppController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controllers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AppController {
+    private static String path = "C:\\Users\\patri\\IdeaProjects\\Sem02\\DatingApp_MiniProject\\src\\main\\java\\com\\example\\demo\\dependency_config.xml";
+    private static final ApplicationContext ctx = new FileSystemXmlApplicationContext(path);
 }
