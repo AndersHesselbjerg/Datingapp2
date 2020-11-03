795fa5b89e376b5625587d0886bc5c23e9c2b351
ande8548@stud.kea.dk <anders_h91@hotmail.com>
2020-11-03 11:47:24 +0100

    Models, repositories

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.models;
+
+
+public class User {
+
+       private String descriptions;
+       private String tags;
+       private int userid;
+       private String userName,
+
+
+
+
+
+
+
+
+}
