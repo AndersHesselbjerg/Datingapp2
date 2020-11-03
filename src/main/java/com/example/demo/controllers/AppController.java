c20bda730ad360baf51ac8f64bc6ee3c4dd0e1e6
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-11-03 11:39:58 +0100

    Changed dependency_config.xml from Absolute to relatve

--- src/main/java/com/example/demo/controllers/AppController.java
+++ src/main/java/com/example/demo/controllers/AppController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controllers;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AppController {
-    private static String path = "C:\\Users\\patri\\IdeaProjects\\Sem02\\DatingApp_MiniProject\\src\\main\\java\\com\\example\\demo\\dependency_config.xml";
-    private static final ApplicationContext ctx = new FileSystemXmlApplicationContext(path);
+    private static String path = "classpath*:dependency_config.xml";
+    private static final ApplicationContext ctx = new ClassPathXmlApplicationContext(path);
 }
