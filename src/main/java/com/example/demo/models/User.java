92de22bd0b8577468397eee59d1acae34dab8e07
ande8548@stud.kea.dk <anders_h91@hotmail.com>
2020-11-03 12:08:20 +0100

    Models, repositories

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -7,9 +7,66 @@ public class User {
        private String tags;
        private int userid;
        private String userName;
+       private String password;
+       private String firstName;
+       private String lastName;
 
 
+       public String getDescription() {
+              return description;
+       }
 
+       public void setDescription(String description) {
+              this.description = description;
+       }
+
+       public String getTags() {
+              return tags;
+       }
+
+       public void setTags(String tags) {
+              this.tags = tags;
+       }
+
+       public int getUserid() {
+              return userid;
+       }
+
+       public void setUserid(int userid) {
+              this.userid = userid;
+       }
+
+       public String getUserName() {
+              return userName;
+       }
+
+       public void setUserName(String userName) {
+              this.userName = userName;
+       }
+
+       public String getPassword() {
+              return password;
+       }
+
+       public void setPassword(String password) {
+              this.password = password;
+       }
+
+       public String getFirstName() {
+              return firstName;
+       }
+
+       public void setFirstName(String firstName) {
+              this.firstName = firstName;
+       }
+
+       public String getLastName() {
+              return lastName;
+       }
+
+       public void setLastName(String lastName) {
+              this.lastName = lastName;
+       }
 
 
 
