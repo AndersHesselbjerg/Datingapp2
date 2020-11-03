c32aaa3f3f59c84661aa64e2b8e1596a1b9a1665
kasper <kasperkbf@hotmail.com>
2020-11-03 11:43:16 +0100

    error html

--- src/main/java/com/example/demo/controllers/AppController.java
+++ src/main/java/com/example/demo/controllers/AppController.java
@@ -4,9 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
     private static String path = "classpath*:dependency_config.xml";
     private static final ApplicationContext ctx = new ClassPathXmlApplicationContext(path);
+
+    @GetMapping("/error")
+    public String error (){
+        return "error";
+    }
 }
