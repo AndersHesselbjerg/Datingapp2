3387116105e166c14f580a1c944c4c7a210ab7ae
kasper <kasperkbf@hotmail.com>
2020-11-03 11:57:09 +0100

    templates

--- src/main/java/com/example/demo/controllers/AppController.java
+++ src/main/java/com/example/demo/controllers/AppController.java
@@ -5,11 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class AppController {
     private static String path = "classpath*:dependency_config.xml";
     private static final ApplicationContext ctx = new ClassPathXmlApplicationContext(path);
+
+    @GetMapping("/")
+    public String index (){
+        return "index";
+    }
 
     @GetMapping("/error")
     public String error (){
